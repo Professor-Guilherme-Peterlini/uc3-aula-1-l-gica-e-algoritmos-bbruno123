bd1a5e2d3b32e62626b6829b46fe002f0502bc3a
bbruno123 <bruno60835396@aluno.es.senac.br>
2025-12-01 18:39:25 +0000

    update

--- src/exercicio2.js
+++ src/exercicio2.js
@@ -1,4 +1,9 @@
-function verificarIdade(i){ return '' }
-module.exports=verificarIdade;
+function verificarIdade (i) {
+    if (i >= 18){
+        return 'maior'
+    }else if (i >= 0){
+        return 'menor'
+    }
+}
 
-
+module.exports = verificarIdade;
